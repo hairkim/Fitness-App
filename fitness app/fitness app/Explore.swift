3912b8dd3208e9d3f64324bd46ec55a7916618c1
Ryan Kim <sunghoonk001@gmail.com>
2024-07-09 20:35:07 -0700

    made posts appear on the explore page

--- fitness app/fitness app/Explore.swift
+++ fitness app/fitness app/Explore.swift
@@ -15,7 +15,7 @@ struct ExploreView: View {
         NavigationView {
             if posts.isEmpty {
                 ProgressView("Loading...")
-                    .onAppear(perform: loadMockData)
+                    .onAppear(perform: loadPosts)
                     .navigationBarHidden(true)
             } else {
                 TabView {
@@ -30,38 +30,17 @@ struct ExploreView: View {
         }
     }
 
-    func loadMockData() {
-        let mockPosts = [
-            Post(
-                id: UUID(),
-                userId: "user1",
-                username: "User 1",
-                imageName: "https://via.placeholder.com/150",
-                caption: "This is a sample post",
-                multiplePictures: false,
-                workoutSplit: "Push",
-                workoutSplitEmoji: "💪",
-                comments: [],
-                date: Date(),
-                likes: 10
-            ),
-            Post(
-                id: UUID(),
-                userId: "user2",
-                username: "User 2",
-                imageName: "https://via.placeholder.com/150",
-                caption: "Another sample post",
-                multiplePictures: false,
-                workoutSplit: "Pull",
-                workoutSplitEmoji: "🏋️‍♂️",
-                comments: [],
-                date: Date(),
-                likes: 5
-            )
-        ]
-
-        DispatchQueue.main.async {
-            posts = mockPosts
+    func loadPosts() {
+        Task {
+            do {
+                let fetchedPosts = try await PostManager.shared.getPosts()
+                DispatchQueue.main.async {
+                    posts = fetchedPosts
+                }
+            } catch {
+                // Handle the error appropriately in your app
+                print("Error fetching posts: \(error)")
+            }
         }
     }
 }
@@ -85,7 +64,7 @@ struct ExploreItemView: View {
                             image
                                 .resizable()
                                 .scaledToFill()
-                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .frame(width: geometry.size.width, height: geometry.size.height * 0.6)
                                 .clipped()
                         case .failure:
                             Color.gray.opacity(0.2)
@@ -147,16 +126,7 @@ struct ExploreItemView: View {
                         }
                         .padding(.horizontal)
                         .sheet(isPresented: $showCommentSheet) {
-                            ExploreCommentsSheetView(
-                                comments: $post.comments,
-                                postId: post.id,
-                                postUser: userStore.currentUser ?? DBUser.placeholder,
-                                currentUser: userStore.currentUser ?? DBUser.placeholder,
-                                deleteComment: { comment in
-                                    post.comments.removeAll { $0.id == comment.id }
-                                },
-                                showCommentSheet: $showCommentSheet
-                            )
+                            CommentSheetView(post: $post)
                         }
 
                         Button(action: {
@@ -190,243 +160,20 @@ struct ExploreItemView: View {
     }
 }
 
-extension DateFormatter {
-    static var shortDateFormatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .short
-        return formatter
-    }
-}
-
-struct ExploreCommentView: View {
-    @Binding var comment: Comment
-    let postId: UUID
-    let postUser: DBUser
-    let deleteComment: (Comment) -> Void
-    let currentUser: DBUser
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            HStack {
-                VStack(alignment: .leading, spacing: 2) {
-                    Text("\(comment.username): \(comment.text)")
-                        .foregroundColor(.primary)
-                    Text(timeAgoSinceDate(comment.date))
-                        .font(.caption)
-                        .foregroundColor(.gray)
-                }
-                Spacer()
-                
-                HStack(spacing: 16) {
-                    Button(action: {
-                        withAnimation {
-                            comment.isReplying.toggle()
-                        }
-                    }) {
-                        Text(comment.isReplying ? "Cancel" : "Reply")
-                            .font(.subheadline)
-                            .foregroundColor(.blue)
-                    }
-                    
-                    if comment.username == currentUser.username {
-                        Menu {
-                            Button(role: .destructive) {
-                                deleteComment(comment)
-                            } label: {
-                                Label("Delete", systemImage: "trash")
-                            }
-                        } label: {
-                            Image(systemName: "ellipsis.circle")
-                                .foregroundColor(.blue)
-                        }
-                    }
-                }
-            }
-            .padding(.vertical, 8)
-
-            if comment.replies.count > 0 {
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        withAnimation {
-                            comment.showReplies.toggle()
-                        }
-                    }) {
-                        Text(comment.showReplies ? "Hide replies" : "View replies")
-                            .font(.subheadline)
-                            .foregroundColor(.blue)
-                    }
-                    Spacer()
-                }
-                .padding(.top, 4)
-            }
-
-            if comment.showReplies {
-                VStack(alignment: .leading, spacing: 4) {
-                    ForEach(comment.replies.indices, id: \.self) { index in
-                        ExploreNestedReplyView(reply: $comment.replies[index], postId: postId, postUser: postUser, deleteComment: { reply in
-                            deleteComment(reply)
-                            comment.replies.removeAll { $0.id == reply.id }
-                        }, currentUser: currentUser, parentUsername: comment.username)
-                        .padding(.leading, 16)
-                    }
-                }
-            }
-
-            if comment.isReplying {
-                HStack {
-                    TextField("Write a reply...", text: $comment.replyText, onCommit: {
-                        Task {
-                            await addReply()
-                        }
-                    })
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.vertical, 8)
-
-                    Button(action: {
-                        Task {
-                            await addReply()
-                        }
-                    }) {
-                        Image(systemName: "arrow.up.circle.fill")
-                            .foregroundColor(.blue)
-                            .imageScale(.large)
-                    }
-                    .padding(.trailing, 16)
-                }
-                .padding(.horizontal, 16)
-            }
-        }
-        .padding(.vertical, 4)
-    }
-
-    private func addReply() async {
-        if !comment.replyText.isEmpty {
-            let newReply = Comment(username: currentUser.username, text: "@\(comment.username) \(comment.replyText)")
-            comment.replies.append(newReply)
-            comment.replyText = ""
-            comment.isReplying = false
-            comment.showReplies = true
-        }
-    }
-}
-
-struct ExploreNestedReplyView: View {
-    @Binding var reply: Comment
-    let postId: UUID
-    let postUser: DBUser
-    let deleteComment: (Comment) -> Void
-    let currentUser: DBUser
-    let parentUsername: String
-    
-    var body: some View {
-        HStack {
-            VStack(alignment: .leading, spacing: 2) {
-                Text("\(reply.username): \(reply.text)")
-                    .foregroundColor(.primary)
-                Text(timeAgoSinceDate(reply.date))
-                    .font(.caption)
-                    .foregroundColor(.gray)
-            }
-            Spacer()
-            
-            HStack(spacing: 16) {
-                Button(action: {
-                    withAnimation {
-                        reply.isReplying.toggle()
-                    }
-                }) {
-                    Text(reply.isReplying ? "Cancel" : "Reply")
-                        .font(.subheadline)
-                        .foregroundColor(.blue)
-                }
-                
-                if reply.username == currentUser.username {
-                    Menu {
-                        Button(role: .destructive) {
-                            deleteComment(reply)
-                        } label: {
-                            Label("Delete", systemImage: "trash")
-                        }
-                    } label: {
-                        Image(systemName: "ellipsis.circle")
-                            .foregroundColor(.blue)
-                    }
-                }
-            }
-        }
-        .padding(.vertical, 4)
-
-        if reply.isReplying {
-            HStack {
-                TextField("Write a reply...", text: $reply.replyText, onCommit: {
-                    Task {
-                        await addReply()
-                    }
-                })
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.vertical, 8)
-
-                Button(action: {
-                    Task {
-                        await addReply()
-                    }
-                }) {
-                    Image(systemName: "arrow.up.circle.fill")
-                        .foregroundColor(.blue)
-                        .imageScale(.large)
-                }
-                .padding(.trailing, 16)
-            }
-            .padding(.horizontal, 16)
-        }
-
-        if reply.showReplies {
-            VStack(alignment: .leading, spacing: 4) {
-                ForEach(reply.replies.indices, id: \.self) { index in
-                    ExploreNestedReplyView(reply: $reply.replies[index], postId: postId, postUser: postUser, deleteComment: { nestedReply in
-                        deleteComment(nestedReply)
-                        reply.replies.removeAll { $0.id == nestedReply.id }
-                    }, currentUser: currentUser, parentUsername: reply.username)
-                    .padding(.leading, 16)
-                }
-            }
-        }
-    }
-
-    private func addReply() async {
-        if !reply.replyText.isEmpty {
-            let newReply = Comment(username: currentUser.username, text: "@\(parentUsername) \(reply.replyText)")
-            reply.replies.append(newReply)
-            reply.replyText = ""
-            reply.isReplying = false
-            reply.showReplies = true
-        }
-    }
-}
-
-struct ExploreCommentsSheetView: View {
-    @Binding var comments: [Comment]
-    let postId: UUID
-    let postUser: DBUser
-    let currentUser: DBUser
-    let deleteComment: (Comment) -> Void
-    @Binding var showCommentSheet: Bool
-    
+struct CommentSheetView: View {
+    @Binding var post: Post
     @State private var newCommentText = ""
+    @Environment(\.dismiss) var dismiss
 
     var body: some View {
         NavigationView {
             VStack {
-                ScrollView {
-                    VStack(alignment: .leading, spacing: 8) {
-                        ForEach(comments.indices, id: \.self) { index in
-                            ExploreCommentView(comment: $comments[index], postId: postId, postUser: postUser, deleteComment: { comment in
-                                deleteComment(comment)
-                                comments.removeAll { $0.id == comment.id }
-                            }, currentUser: currentUser)
-                            .padding(.horizontal, 16)
-                            .padding(.top, 8)
+                List {
+                    ForEach(post.comments.indices, id: \.self) { index in
+                        VStack(alignment: .leading) {
+                            Text(post.comments[index].username)
+                                .font(.headline)
+                            Text(post.comments[index].text)
                         }
                     }
                 }
@@ -435,37 +182,38 @@ struct ExploreCommentsSheetView: View {
                     TextField("Add a comment...", text: $newCommentText)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-
-                    Button(action: {
-                        Task {
-                            await addComment()
-                        }
-                    }) {
+                    
+                    Button(action: addComment) {
                         Image(systemName: "arrow.up.circle.fill")
                             .foregroundColor(.blue)
                             .imageScale(.large)
+                            .padding()
                     }
-                    .padding(.trailing, 16)
                 }
-                .padding(.horizontal, 16)
-                .padding(.vertical, 8)
-                .background(Color.white)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .padding()
             }
             .navigationBarTitle("Comments", displayMode: .inline)
             .navigationBarItems(trailing: Button("Done") {
-                showCommentSheet = false
+                dismiss()
             })
         }
     }
 
-    private func addComment() async {
-        if !newCommentText.isEmpty {
-            let newComment = Comment(username: currentUser.username, text: newCommentText)
-            comments.append(newComment)
-            newCommentText = ""
-        }
+    private func addComment() {
+        guard !newCommentText.isEmpty else { return }
+        let newComment = Comment(username: "Current User", text: newCommentText)
+        post.comments.append(newComment)
+        newCommentText = ""
+    }
+}
+
+extension DateFormatter {
+    static var shortDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter
     }
 }
 
